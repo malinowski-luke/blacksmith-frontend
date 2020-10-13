3f13b2564b5efd1fc77d0b9b64924c049b85fb3f
Luke Malinowski <malinowski.luke123@gmail.com>
2020-10-13 17:01:14 -0400

    set up mongoose and mongo db connection

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
-require('dotenv').config({})
+require('dotenv').config({path:'./.env.local'})
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
 const Router = require('./router/index')
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT 
+const MONGO_URI = process.env.MONGO_URI
 
+// console.log(PORT, MONGO_URI)
 app.use(cors())
 app.use(express.json())
 app.use(Router)
 
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+},(err)=>{
+  if(err) console.error(err)
+})
+const db = mongoose.connection
 
-app.listen(PORT, () => console.log(`app running on port: ${PORT}`))
+
+
+
+app.listen(PORT, '0.0.0.0',() => console.log(
+  `app running on port: ${PORT}`,
+  `\nconnected to mongo ${db.name} DB`
+))
